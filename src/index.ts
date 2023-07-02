3c4cca7924aea7667752c8af933e94ef7000d288
Vadzim.Velikanets <velikanets@mail.ru>
2023-07-02 22:22:40 +0300

    Add login and registration

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import dotenv from "dotenv";
 import app from "./app";
 import corsMiddleware from "./middleware/cors.middleware";
+import authRouter from "./auth/auth.router";
 import * as process from "process";
 import * as mongoose from "mongoose";
 
 dotenv.config()
+mongoose.set("strictQuery", false);
 
 const PORT =  process.env.PORT || 8080;
 app.use(corsMiddleware);
+app.use('/auth', authRouter);
+
 const start = async () => {
     try {
         await mongoose.connect(process.env.DB_URI as string)
