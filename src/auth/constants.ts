425179cfd9636578a56aa0375bd0d622fc980188
Vadzim.Velikanets <velikanets@mail.ru>
2023-07-05 20:24:10 +0300

    Update Auth and add constants

---
+++ src/auth/constants.ts
@@ -0,0 +1,9 @@
+export enum EAuth {
+    REGISTRATION_ERROR = "Registration error",
+    USER_EXIST = "User already exists",
+    USER_CREATED = "User created!",
+    USER_NOT_EXIST = "User does not exist!",
+    LOGIN_ERROR = "Login Error",
+    AUTH_ERROR = "Auth Error",
+    WRONG_PASSWORD = "Incorrected password!"
+}
