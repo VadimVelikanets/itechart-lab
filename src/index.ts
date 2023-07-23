6de925e315c47749e0ea5a0ebf866503f6010d8a
Vadzim.Velikanets <velikanets@mail.ru>
2023-07-23 17:25:51 +0300

    feat - add linter in logger in project.

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,26 @@
-import dotenv from "dotenv";
-import app from "./app";
-import corsMiddleware from "./middleware/cors.middleware";
-import authRouter from "./auth/auth.router";
-import * as process from "process";
-import * as mongoose from "mongoose";
+import * as process from 'process';
+import * as mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-dotenv.config()
-mongoose.set("strictQuery", false);
+import app from './app';
+import corsMiddleware from './middleware/cors.middleware';
+import authRouter from './auth/auth.router';
+import logger from './logger';
 
-const PORT =  process.env.PORT || 8080;
+dotenv.config();
+mongoose.set('strictQuery', false);
+
+const PORT = process.env.PORT || 8080;
 app.use(corsMiddleware);
 app.use('/auth', authRouter);
 
 const start = async () => {
-    try {
-        await mongoose.connect(process.env.DB_URI as string)
-        app.listen(PORT, () => console.log(`Server started on port ${process.env.PORT}`) )
-    } catch (e) {
-        console.log(e)
-    }
-}
+  try {
+    await mongoose.connect(process.env.DB_URI as string);
+    app.listen(PORT, () => logger.info(`Server started on port ${process.env.PORT}`));
+  } catch (e) {
+    logger.error(e);
+  }
+};
 
 start();
